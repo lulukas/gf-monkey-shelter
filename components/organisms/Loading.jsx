9217dacbb310f326d981e684a9b7404867b30e03
Lukas <lukas.klier@oscillate.ch>
2021-04-03 09:57:46 +0200

    updated loading screen

---
+++ components/organisms/Loading.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+// Components
+import { Banana } from '../atoms/Banana'
+
+// Styles
+import styles from '../../styles/Loading.module.css'
+
+export const Loading = () => {
+  return (
+    <div className={styles.container}>
+      <p className={styles.text}>We are loading your monkeys!!</p>
+      <div className={styles.banana}>
+        <Banana size={80} />
+      </div>
+    </div>
+  )
+}
--- components/pages/Report.jsx
+++ components/pages/Report.jsx
@@ -3,15 +3,24 @@ import React, { Fragment } from 'react'
 // Hooks
 import { useMonkeys } from '../../hooks/useMonkeys'
 
+// Components
+import { Loading } from '../organisms/Loading'
+
 // Styles
 import styles from '../../styles/Report.module.css'
+import { Header } from '../organisms/report/Header'
+import { getMonkeysBySpecies } from '../../utils/getMonkeysBySpecies'
+import { SectionSpecies } from '../organisms/report/SectionSpecies'
 
 export const Report = () => {
   const { monkeys, loading, err } = useMonkeys()
-  console.log("🚀 ~ file: Report.jsx ~ line 6 ~ Report ~ monkeys", monkeys)
+
+  const monkeysBySpecies = monkeys && getMonkeysBySpecies(monkeys)
+  console.log('🚀 ~ file: Report.jsx ~ line 18 ~ Report ~ monkeysBySpecies', monkeysBySpecies)
+
   return (
     <div className={styles.container}>
-      {(loading && <h1>Loading ...</h1>) ||
+      {(loading && <Loading />) ||
         (err && (
           <Fragment>
             <h1>Err</h1>
@@ -19,10 +28,12 @@ export const Report = () => {
           </Fragment>
         )) || (
           <Fragment>
-            <h1>My Monkeys</h1>
-            {monkeys.map((monkey) => (
-              <p>{JSON.stringify(monkey)}</p>
-            ))}
+            <Header monkeys={monkeys} />
+            <div>
+              {monkeysBySpecies.map(({ specie, monkeys }) => (
+                <SectionSpecies specie={specie} monkeys={monkeys} />
+              ))}
+            </div>
           </Fragment>
         )}
     </div>
